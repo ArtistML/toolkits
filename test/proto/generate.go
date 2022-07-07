79ff00cdb68f05725de65c37da4da06adf5190db
Sylvan <sylvansu@gmail.com>
2022-07-07 15:02:52 +0800

    Feature: add filters for gen-proto (#1)
    Co-authored-by: sylvan <sylvan2future@gmail.com>

--- test/proto/generate.go
+++ test/proto/generate.go
@@ -1,3 +1,3 @@
 package echo
 
-//go:generate go run "github.com/artistml/toolkits/cmd/gen-proto" --host=github.com/artistml/toolkits
+//go:generate go run "github.com/artistml/toolkits/cmd/gen-proto" --host=github.com/artistml/toolkits -f echo/v2
